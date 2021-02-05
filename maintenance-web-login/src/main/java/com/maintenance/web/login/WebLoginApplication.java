5a2adc3ca4c7d6b2935b10077f5405ce6ce8dcca
liangxincheng <lxcldh0324@163.com>
2021-02-05 12:43:53 +0800

    添加拦截器

--- maintenance-web-login/src/main/java/com/maintenance/web/login/WebLoginApplication.java
+++ maintenance-web-login/src/main/java/com/maintenance/web/login/WebLoginApplication.java
@@ -3,10 +3,11 @@ package com.maintenance.web.login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@Component(value = "com.maintenance.web")
+@ComponentScan(value = "com.maintenance.web")
 public class WebLoginApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebLoginApplication.class, args);
